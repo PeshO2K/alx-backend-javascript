3576a9786e4458580f0538589e135ca8e6857d4a
PeshO2K <patienceotuke@gmail.com>
2024-04-18 12:40:43 +0300

    Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((userResponse, photoResponse) => [
+    { status: userResponse.status, value: userResponse.value },
+    { status: photoResponse.status, value: photoResponse.value },
+  ]);
+}
