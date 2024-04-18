dae63fbd87a6fbe673d96f4d3d295fc5dec64f3d
PeshO2K <patienceotuke@gmail.com>
2024-04-18 09:59:02 +0300

    Task 2 : error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch(() => Error());
+    .catch(() => new Error());
 }
 
 export default handleResponseFromAPI;
