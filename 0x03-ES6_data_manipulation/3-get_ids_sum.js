b9e43059f17362417d29606eec3cd7dd070fddde
PeshO2K <patienceotuke@gmail.com>
2024-05-14 18:57:16 +0300

    TAsk 3 updated

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
-      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      (sumIds, curStudent) => sumIds + curStudent.id,
       0,
     );
   }
